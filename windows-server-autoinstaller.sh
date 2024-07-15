9edae3803370bbec9b4ef76407d0680a3c2ab5ff
Erlkim <99765959+erlkim@users.noreply.github.com>
2024-07-16 02:04:27 +0700

    Update windows-server-autoinstaller.sh

--- windows-server-autoinstaller.sh
+++ windows-server-autoinstaller.sh
@@ -59,7 +59,7 @@ esac
 echo "Selected Windows Server version: $img_file"
 
 # Create a raw image file with the chosen name
-qemu-img create -f raw "$img_file" 100G
+qemu-img create -f raw "$img_file" 120G
 
 echo "Image file $img_file created successfully."
 
